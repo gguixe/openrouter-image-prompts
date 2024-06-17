171a59a8ac63c4dba24d3400f89c35bfaef58ab5
Rat <gguixe23@gmail.com>
2024-06-17 20:31:17 +0200

    Added more error management & app info

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ import time
 
 SHARED_FOLDER = 'shared'
 OPENROUTER_API_KEY = ''
+YOUR_SITE_URL = 'https://github.com/gguixe/openrouter-image-prompts'
+YOUR_APP_NAME = 'openrouter-image-prompts'
 MODEL = 'microsoft/phi-3-medium-128k-instruct:free'
 PROMPT = 'A cute random animal in a random artistic style. Repeat this previous prompt but changing the random words'
-INTERVAL = 8 * 60 * 60  # Interval in seconds (8 hours)
+INTERVAL = 8 * 3600  # Interval in seconds (8 hours)
 
 def fetch_and_store_message():
 	try:
-		print("Request to " + MODEL)
-		print("Prompt: " + PROMPT)
+		print("Request to " + f"{MODEL}")
+		print("Prompt: " + f"{PROMPT}")
+		
 		response = requests.post(
 		  url="https://openrouter.ai/api/v1/chat/completions",
 		  headers={
 			"Authorization": f"Bearer {OPENROUTER_API_KEY}",
+			"HTTP-Referer": f"{YOUR_SITE_URL}", 
+			"X-Title": f"{YOUR_APP_NAME}", 
 		  },
 		  data=json.dumps({
 			"model": f"{MODEL}", 
@@ -28,22 +33,26 @@ def fetch_and_store_message():
 		
 		if response.status_code == 200:
 			data = response.json()
+			
 			message = data['choices'][0]['message']['content']
 			print("Response:")
 			print(message)
 			
-			shared_folder_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "{SHARED_FOLDER}")
+			shared_folder_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), f"{SHARED_FOLDER}")
 			file_path = os.path.join(shared_folder_path, "prompt.txt")
-			
+			print(f"Message stored at {file_path}")
+
 			with open("prompt.txt", "w") as file:
 				file.write(message)
+		elif response.status_code == 429: 
+			print(f"Free tokens limit reached -> Error: " +  str(response.status_code))
 		else:
-			print(f"Error: response.status_code")
+			print(f"Error: " + response.status_code)
 	except requests.exceptions.RequestException as e:
 		print(f"Request error: {e}")
 
 # Main loop to run the request every 8 hours
 while True:
     fetch_and_store_message()
-    print("Next prompt in " + f"{INTERVAL}" + " seconds")
+    print("Next prompt in " + f"{INTERVAL/3600}" + " hours")
     time.sleep(INTERVAL)
